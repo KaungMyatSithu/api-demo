1bd921ed854c16fe05ea7d6ecf3d8fa1c67a735c
KaungMyatSithu <116023059+KaungMyatSithu@users.noreply.github.com>
2023-12-19 09:32:07 +0630

    Initial commit

---
+++ src/main/java/org/ace/apidemo/controller/UserController.java
@@ -0,0 +1,33 @@
+package org.ace.apidemo.controller;
+
+import org.ace.apidemo.entity.User;
+import org.ace.apidemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/save")
+    public ResponseEntity<User> save(@RequestBody User user){
+        userService.save(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+    @GetMapping("/findAll")
+    public ResponseEntity<List<User>> findAll(){
+        List<User> userList = userService.findAll();
+        return new ResponseEntity<>(userList,HttpStatus.OK);
+    }
+
+
+}
